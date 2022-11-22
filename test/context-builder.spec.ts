89acf96aded234208e83fb9f01fb2b6e071295cf
Gintautas Miselis <gintautas@miselis.lt>
2022-11-22 11:12:16 +0200

    Fix broken context-builder test

--- test/context-builder.spec.ts
+++ test/context-builder.spec.ts
@@ -1,70 +1,50 @@
-import { createContextForFunction } from '../lib';
-import { BindingDefinition, Context } from '@azure/functions';
+import { Binding, createContextForFunction } from '../lib';
+import { BindingDefinition, Context, ContextBindings } from '@azure/functions';
 import { expect } from 'chai';
-import { stub } from 'sinon';
+
+class MyTrigger implements Binding
+{
+    toContextBinding(): ContextBindings {
+        return {};
+    }
+
+    toTrigger(): ContextBindings | string {
+        return {};
+    }
+}
 
 describe('context-builder', () => {
     it('creates a context for a function', () => {
         const bindingDefinitions: BindingDefinition[] = [{ name: "myTrigger", type: "manualTrigger", direction: "in" }];
-        const augmentStub = stub();
         const context = createContextForFunction(
             function myTestFunction(context: Context) {
                 context.log('test');
             },
             bindingDefinitions,
-            { myTrigger: {} },
-            augmentStub,
+            { myTrigger: new MyTrigger() },
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            () => {},
         );
-        const { invocationId } = context;
+        const { done, invocationId, log } = context;
+
         expect(context).to.deep.equal({
             invocationId,
             bindingData: { invocationId },
             bindingDefinitions,
-            bindings: {},
+            bindings: {
+                myTrigger: {},
+            },
+            done,
             executionContext: {
                 functionDirectory: context.executionContext.functionDirectory,
                 functionName: 'myTestFunction',
                 invocationId,
                 retryContext: null,
             },
-            log: context.log,
+            log,
             traceContext: {
                 attributes: null,
                 traceparent: null,
-                tracestate: null,
-            },
-        });
-        expect(augmentStub).to.have.callCount(1);
-        expect(augmentStub).to.have.been.calledWith(context);
-    });
-    it('creates a context and augments it', () => {
-        const bindingDefinitions: BindingDefinition[] = [{ name: "myTrigger", type: "manualTrigger", direction: "in" }];
-        const context = createContextForFunction(
-            function myTestFunction(context: Context) {
-                context.log('test');
-            },
-            bindingDefinitions,
-            { myTrigger: {} },
-            (context: Omit<Context, 'done'>) => {
-                context.traceContext.traceparent = '123';
-            },
-        );
-        const { invocationId } = context;
-        expect(context).to.deep.equal({
-            invocationId,
-            bindingData: { invocationId },
-            bindingDefinitions,
-            bindings: {},
-            executionContext: {
-                functionDirectory: context.executionContext.functionDirectory,
-                functionName: 'myTestFunction',
-                invocationId,
-                retryContext: null,
-            },
-            log: context.log,
-            traceContext: {
-                attributes: null,
-                traceparent: '123',
                 tracestate: null,
             },
         });
